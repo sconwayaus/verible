3e37d7b4415bdf8e3d073d813a85f94bb63e2293
sconwayaus <stevenconwayaus@gmail.com>
2026-01-30 00:02:03 +1100

    Adding a lint rule to use always_ff/comb/latch over verilog always blocks.

--- verible/verilog/tools/lint/testdata/generate_begin_module.sv
+++ verible/verilog/tools/lint/testdata/generate_begin_module.sv
@@ -2,6 +2,7 @@ module generate_begin_module;
   // verilog_lint: waive legacy-generate-region
   generate
     begin : gen_block1
+    // verilog_lint: waive always-block
       always @(posedge clk) begin
         foo <= bar;
       end
